d9c6368b9e4eebbfde11578e33a9f306a2bcddfe
Barnabas Bulpett <barnabasbulpett@gmail.com>
2022-10-22 08:09:00 -0400

    Endpoints/views for Show/Update/Delete

--- drinks/views.py
+++ drinks/views.py
@@ -9,7 +9,8 @@ from rest_framework import status
 @api_view(['GET', 'POST'])
 
 # Create endpoints
-def drink_list(request):
+# (allow format to show raw JSON in browser)
+def drink_list(request, format=None):
 
     # GET request to "Read" all the drinks
     if request.method == 'GET':
@@ -30,3 +31,37 @@ def drink_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status.HTTP_201_CREATED)
+
+
+# Create view to show specific model ("show page"), update, or delete
+@api_view(['GET', 'PUT', 'DELETE'])
+
+# (allow format to show raw JSON in browser)
+def drink_detail(request, id, format=None):
+
+    try:
+        drink = Drink.objects.get(pk=id)  # Primary key = ID parameter
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+	# GET request to show specific object
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+
+        return Response(serializer.data)
+	# PUT request to edit/update specific object
+    elif request.method == 'PUT':
+        # Accepts JSON request data
+        serializer = DrinkSerializer(drink, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        # Respond with Bad Request if invalid
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	# DELETE request to delete specific object
+    elif request.method == 'DELETE':
+        drink.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
